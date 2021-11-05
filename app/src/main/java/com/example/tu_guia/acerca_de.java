530285138c601218372402bb65a726b5717c4888
Profe Leidy León <leidyleon0208ita@gmail.com>
2021-11-05 17:33:08 -0500

    Primer commit

---
+++ app/src/main/java/com/example/tu_guia/acerca_de.java
@@ -0,0 +1,14 @@
+package com.example.tu_guia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class acerca_de extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de);
+    }
+}
