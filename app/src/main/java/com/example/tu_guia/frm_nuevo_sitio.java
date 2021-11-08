0e379e287f1367fce936e13543fb71cec0c40e5b
Profe Leidy León <leidyleon0208ita@gmail.com>
2021-11-07 20:58:52 -0500

    formulario para agregar sitio

---
+++ app/src/main/java/com/example/tu_guia/frm_nuevo_sitio.java
@@ -0,0 +1,14 @@
+package com.example.tu_guia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class frm_nuevo_sitio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_nuevo_sitio);
+    }
+}
