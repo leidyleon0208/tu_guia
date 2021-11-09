ef540c22f8980ce6a4895fa7f90c049e07f72cbf
Profe Leidy León <leidyleon0208ita@gmail.com>
2021-11-08 20:37:01 -0500

    Se agrega nueva actividad para busqueda por region

---
+++ app/src/main/java/com/example/tu_guia/busqueda_region.java
@@ -0,0 +1,14 @@
+package com.example.tu_guia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class busqueda_region extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busqueda_region);
+    }
+}
